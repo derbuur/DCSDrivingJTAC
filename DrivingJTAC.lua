f98700ea24a068d9f7032d0b77e845a62c16ca73
der buur <derbuur@googlemail.com>
2022-01-30 18:55:54 +0100

    Add files via upload
    - solved bug with wrong cardinal. was a typo in code
    - added T00 Marks for all coalitions attached to the bullseye. So it should work on every map

--- DrivingJTAC.lua
+++ DrivingJTAC.lua
@@ -1,5 +1,8 @@
 -- Driving JTAC by buur
 
+-- trigger.action.markToCoalition( 1 , "T00" , {0,0,0} , 2 , true) -- dummy target, will not shown in target list
+-- trigger.action.markToCoalition( 2 , "T00" , {0,0,0} , 0 , true) -- dummy target, will not shown in target list
+-- trigger.action.markToCoalition( 3 , "T00" , {0,0,0} , 1 , true) -- dummy target, will not shown in target list
 
 
 function s(table)
@@ -12,13 +15,13 @@ end
  Cardinals = function(angle)
       if     angle <= 22.5  and angle >= 0 then return "N"
 	  elseif angle <= 360   and angle > 337.5 then return "N"
-	  elseif angle <= 67.5  and angle > 22.5  then return "NW"
-	  elseif angle <= 112.5 and angle > 67.5  then return "W"
-	  elseif angle <= 157.5 and angle > 112.5 then return "SW"
+	  elseif angle <= 67.5  and angle > 22.5  then return "NE"
+	  elseif angle <= 112.5 and angle > 67.5  then return "E"
+	  elseif angle <= 157.5 and angle > 112.5 then return "SE"
 	  elseif angle <= 202.5 and angle > 157.5 then return "S"
-	  elseif angle <= 247.5 and angle > 202.5 then return "SE"
-	  elseif angle <= 292.5 and angle > 247.5 then return "E"
-	  elseif angle <= 337.5 and angle > 292.5 then return "NE"
+	  elseif angle <= 247.5 and angle > 202.5 then return "SW"
+	  elseif angle <= 292.5 and angle > 247.5 then return "W"
+	  elseif angle <= 337.5 and angle > 292.5 then return "NW"
 	  else
 	  env.info("angle higher 360")
 	  end
@@ -26,7 +29,21 @@ end
 
 active_requests = {}
 DrivingJTACShowRequest = {}
-  
+
+
+	local BullsCoordinateBlue = COORDINATE:NewFromVec3( coalition.getMainRefPoint( 2 ) )
+	local BullsCoordinateRed = COORDINATE:NewFromVec3( coalition.getMainRefPoint( 1 ) )
+	local BullsCoordinateGreen = COORDINATE:NewFromVec3( coalition.getMainRefPoint( 0 ) )
+
+	--local Coordinate=AIRBASE:FindByName("Batumi"):GetCoordinate() -- dummy coordinate for AV-8B ATHS
+	MARKER:New(BullsCoordinateBlue, "T00"):ToCoalition(2) -- dummy Zone for AV-8B ATHS
+	MARKER:New(BullsCoordinateRed, "T00"):ToCoalition(1)
+	MARKER:New(BullsCoordinateGreen, "T00"):ToCoalition(0)
+	
+--s(Coordinate)
+--env.info("coordinate Batumi "..Coordinate) 
+
+
 --local MenuDrivingJTACRed = MENU_COALITION:New( coalition.side.RED, "Driving JTAC" )
 --local MenuDrivingJTACBlue = MENU_COALITION:New( coalition.side.BLUE, "Driving JTAC" )
 local MenuDrivingJTAC = MENU_MISSION:New( "Driving JTAC" )
@@ -102,13 +119,13 @@ drivingJTAC = function(JTAC,RecceZone)--,ATHS_Marker)
 	--local targetVelocityText = tostring(targetVelocity)
 	--local targetVelocityText = "dummy"
 	local targetDescription = unitsGroup .. " - " .. targetVelocityText.." "..targetHeadingCardinals
-
+--env.info("target Heading: " ..targetHeading)
 	  local distance = math.floor((self:GetCoordinate():Get2DDistance(self:GetDetectedUnits():GetCoordinate()))*1.09361+0.5)
 	  local angle = self:GetDetectedUnits():GetCoordinate():GetAngleDegrees(self:GetDetectedUnits():GetCoordinate():GetDirectionVec3(self:GetCoordinate()))
 	  local angle = Cardinals(angle)
 	  local text_NineLiner = string.format(" Message from %s \n Request Nr.: %d \n 4. ELEV %d \n 5. DESC %s \n     %s \n 6. %s \n     %s \n 7. Laser %d \n 8. FRND %s %s", self:GetName(), request ,(Group:GetHeight())*3.28084, currentthreattype, targetDescription, group:GetCoordinate():ToStringMGRS( nil ), group:GetCoordinate():ToStringLLDMS( nil ) , self:GetLaserCode(), distance, angle )
 	  local text_JTAC = string.format("%s: has new request %d", self:GetName(), request)
-	  MESSAGE:New(text_JTAC, 30):ToCoalition(self:GetCoalition()) --:ToAll()
+	  MESSAGE:New(text_JTAC, 30):ToCoalition(self:GetCoalition())
 
 
 	-- einfuegung text nur auf einer Seite zu sehen
@@ -133,7 +150,7 @@ drivingJTAC = function(JTAC,RecceZone)--,ATHS_Marker)
 	  
 	  
 	
-	  
+	  local menuHandle = {}
 	  local Clients = SET_CLIENT:New():FilterCoalitions(coaltitionName):FilterStart()
 	  
 		Clients:ForEachClient(function (MenuClient)
@@ -141,15 +158,13 @@ drivingJTAC = function(JTAC,RecceZone)--,ATHS_Marker)
 				local group = MenuClient:GetGroup()
 				local groupName = group:GetName()
 
-				env.info("Player MenuClient NAME: " ..group:GetName())
-				--menuManage = MENU_GROUP:New(group,"Bla und Blub")
-				--MENU_GROUP_COMMAND:New( group, "Show Nine Liner", MenuManage, showNineLiner, group )
-				DrivingJTACShowRequest[group] = MENU_COALITION_COMMAND:New( self:GetCoalition(), "Show Request "..request , MenuDrivingJTAC, showNineLiner, group) --ShowStatus, "Status of planes is ok!", "Message to Red Coalition" )
-				DrivingJTACShowRequest = DrivingJTACShowRequest[group]
+				menuHandle[group] = MENU_COALITION_COMMAND:New( self:GetCoalition(), "Show Request "..request , MenuDrivingJTAC, showNineLiner, group)
+
 
 			end
 		end)
-	
+		
+	DrivingJTACShowRequest = {[request] = {menuHandle}}
 	-- einfuegung ende
 		
 	  --DrivingJTACShowRequest = MENU_COALITION_COMMAND:New( self:GetCoalition(), "Show Request "..request , MenuDrivingJTACBlue, showNineLiner) --ShowStatus, "Status of planes is ok!", "Message to Red Coalition" )
@@ -167,20 +182,25 @@ drivingJTAC = function(JTAC,RecceZone)--,ATHS_Marker)
 
 	--- Function called whenever a dected group could not be detected anymore.
 	function drivingJTAC:OnAfterDetectedGroupLost(From, Event, To, Group)
-	  local group=Group
-	  local unitsGroup = self:GetDetectedUnits():GetTypeNames()
-	  
-		
-	  
-	  local text=string.format("%s: LOST Request %d, abort", self:GetName(), active_requests[self]["request"])--active_requests.self.request)
-	  MESSAGE:New(text, 30):ToCoalition(self:GetCoalition()) --:ToAll()
-	  env.info(text)
+	  local unitsGroup = self:GetDetectedUnits():GetTypeNames()  
+	  local text=string.format("%s: LOST Request %d, abort", self:GetName(), active_requests[self]["request"])
+	  MESSAGE:New(text, 30):ToCoalition(self:GetCoalition())
+	  --env.info(text)
 		self:LaserOff()
 		self:RemoveWaypointByID(wpDetection.uid)
 		self:Cruise()
 		
 		active_requests[self]["mymarker"]:Remove()
-		active_requests[self]["ShowRequest"]:Remove()
+		request = active_requests[self]["request"]
+
+		local importantTable = {}
+		importantTable = active_requests[self]["ShowRequest"]
+		for index, value in pairs(importantTable[request]) do
+			value:Remove()
+		end
+		
+		
+		
 	end
 
 	--- Function called when the group enteres a zone.
@@ -224,8 +244,7 @@ drivingJTAC = function(JTAC,RecceZone)--,ATHS_Marker)
 end
 
 
-local Coordinate=AIRBASE:FindByName("Batumi"):GetCoordinate() -- dummy coordinate for AV-8B ATHS
-local mymarker=MARKER:New(Coordinate, "T00"):ToCoalition(2) -- dummy Zone for AV-8B ATHS
+
 
 drivingJTAC("BAYONET","Recce BAYONET")
 drivingJTAC("Axeman","Recce Axeman")
